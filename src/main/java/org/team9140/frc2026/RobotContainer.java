74db2e91c79d901a4cab2d7384bd62a99e85ca5b
Shaurya Vatsal <vatshaurya@gmail.com>
2026-05-20 18:31:41 -0500

    reorganize + install dependencies

--- src/main/java/org/team9140/frc2026/RobotContainer.java
+++ src/main/java/org/team9140/frc2026/RobotContainer.java
@@ -2,12 +2,12 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot;
+package org.team9140.frc2026;
 
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+import org.team9140.frc2026.Constants.OperatorConstants;
+import org.team9140.frc2026.commands.Autos;
+import org.team9140.frc2026.commands.ExampleCommand;
+import org.team9140.frc2026.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
