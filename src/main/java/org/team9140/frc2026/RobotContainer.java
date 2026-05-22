978a4c7e9ed7ed24e2ac229323305aa4dfcf8e7e
Naman Shah <namanmshah09@gmail.com>
2026-05-21 20:34:28 -0500

    removed autogenerated constant

--- src/main/java/org/team9140/frc2026/RobotContainer.java
+++ src/main/java/org/team9140/frc2026/RobotContainer.java
@@ -4,7 +4,6 @@
 
 package org.team9140.frc2026;
 
-import org.team9140.frc2026.Constants.OperatorConstants;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -19,7 +18,7 @@ public class RobotContainer {
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+      new CommandXboxController(0);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
