c1d300f66fea36c230d187f8558fdd5e12ffffc6
Shaurya Vatsal <vatshaurya@gmail.com>
2026-05-22 09:41:03 -0500

    Write the Extender implemenation

---
+++ src/main/java/org/team9140/frc2026/subsystems/Extender/Extender.java
@@ -0,0 +1,44 @@
+package org.team9140.frc2026.subsystems.Extender;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import org.littletonrobotics.junction.Logger;
+import org.team9140.frc2026.Constants;
+
+public class Extender extends SubsystemBase{
+    ExtenderIO extender;
+    ExtenderIOInputsAutoLogged inputs = new ExtenderIOInputsAutoLogged();
+
+    public Extender(ExtenderIO extender) {
+        this.extender = extender;
+    }
+
+    @Override
+    public void periodic() {
+        extender.updateInputs(inputs);
+        Logger.processInputs("Extender", inputs);
+    }
+
+    public Command armIn() {
+        return this.runOnce(() -> {
+            extender.setPosition(
+                Constants.Extender.ARM_IN_POSITION, 
+                Constants.Extender.ARM_IN_VELOCITY);
+        });
+    }
+
+    public Command armOut() {
+        return this.runOnce(() -> {
+            extender.setPosition(
+                Constants.Extender.ARM_OUT_POSITION, 
+                Constants.Extender.ARM_OUT_VELOCITY);
+        });
+    }
+
+    public Command armInOutLoop() {
+        return this.run(() -> {
+            armIn().withTimeout(1).andThen(armOut().withTimeout(1)).repeatedly();
+        });
+    }
+}
