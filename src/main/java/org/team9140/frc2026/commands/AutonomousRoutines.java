4a78c9e574ee5f7570a4bece20e1a0e9ccfeee29
Naman Shah <namanmshah09@gmail.com>
2026-05-30 23:48:28 -0500

    Refactored auto with auto factory

--- src/main/java/org/team9140/frc2026/commands/AutonomousRoutines.java
+++ src/main/java/org/team9140/frc2026/commands/AutonomousRoutines.java
@@ -1,10 +1,7 @@
 package org.team9140.frc2026.commands;
 
-import java.util.Optional;
-
 import org.littletonrobotics.junction.AutoLogOutput;
-import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
-import org.team9140.frc2026.Robot;
+import org.littletonrobotics.junction.Logger;
 import org.team9140.frc2026.subsystems.drive.CommandSwerveDrivetrain;
 import org.team9140.frc2026.subsystems.extender.Extender;
 import org.team9140.frc2026.subsystems.feeder.Feeder;
@@ -13,31 +10,34 @@ import org.team9140.frc2026.subsystems.roller.Roller;
 import org.team9140.frc2026.subsystems.shooter.Shooter;
 import org.team9140.frc2026.subsystems.spinner.Spinner;
 import org.team9140.frc2026.subsystems.turret.Turret;
-import org.team9140.lib.Util;
 
-import choreo.Choreo;
+import choreo.auto.AutoChooser;
+import choreo.auto.AutoFactory;
+import choreo.auto.AutoRoutine;
+import choreo.auto.AutoTrajectory;
 import choreo.trajectory.SwerveSample;
 import choreo.trajectory.Trajectory;
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class AutonomousRoutines {
-    CommandSwerveDrivetrain drivetrain;
-    Turret turret;
-    Shooter shooter;
-    Hood hood;
-    Feeder feeder;
-    Spinner spinner;
-    Extender extender;
-    Roller roller;
+    private CommandSwerveDrivetrain drivetrain;
+    private Turret turret;
+    private Shooter shooter;
+    private Hood hood;
+    private Feeder feeder;
+    private Spinner spinner;
+    private Extender extender;
+    private Roller roller;
 
-    enum Autos {
-        TEST // example, should be deleted later
-    }
+    private final Trigger readyToShoot;
 
-    LoggedDashboardChooser<Autos> autoChooser = new LoggedDashboardChooser<>("Auto chooser");
+    private final AutoFactory autoFactory;
+    private final AutoChooser autoChooser;
 
     public AutonomousRoutines(CommandSwerveDrivetrain drivetrain,
                  Turret turret,
@@ -56,56 +56,102 @@ public class AutonomousRoutines {
         this.extender = extender;
         this.roller = roller;
 
-        autoChooser.addOption("Testing", Autos.TEST);
+        Command resetAllSubsystems = Commands.parallel(
+            this.drivetrain.stop(),
+            this.turret.off(),
+            this.shooter.off(),
+            this.hood.off(),
+            this.feeder.reverseAndOff(),
+            this.spinner.off(),
+            this.roller.off()
+        );
+
+        this.readyToShoot = turret.atPosition.and(hood.atPosition).and(shooter.atVelocity);
+
+        this.autoFactory = new AutoFactory(this.drivetrain::getDrivetrainPose,
+            this.drivetrain::resetPose, 
+            this.drivetrain::followSample, 
+            true, 
+            this.drivetrain, 
+            (Trajectory<SwerveSample> traj, Boolean end) -> {
+                // Documentation doesn't say if end=true means traj starts or traj ends so this is a guess
+                Logger.recordOutput("Current trajectory is starting", end);
+                Logger.recordOutput("Current trajectory name", traj.name());
+            }
+            );
+        
+        this.autoChooser = new AutoChooser("Do Nothing");
+
+        autoChooser.addCmd("Shoot Preload", this::shootPreload);
+        autoChooser.addRoutine("One Pass Over Bump From Left", this::onePassBumpDepot);
+
+        SmartDashboard.putData("Auto Chooser", this.autoChooser);;
+        RobotModeTriggers.autonomous().whileTrue(autoChooser.selectedCommandScheduler());
+        // Reset everything at the end of auto so no commands outlive auto
+        // Default commands would probably get rid of the need for this
+        RobotModeTriggers.autonomous().onFalse(resetAllSubsystems);
     }
 
-    private Command autonCommand = null;
     @AutoLogOutput
     private Pose2d initialAutoPose = null;
 
-    // For each auto you have to do 3 things:
-    // 1. load all needed trajectories
-    // 2. set the intial pose
-    // 3. create the actual sequence of commands
-    private void updateAuto(Autos selectedAuto) {
-        autonCommand = switch (selectedAuto) {
-            case TEST -> {
-                Trajectory<SwerveSample> traj = loadTrajectory("Bump_Depot_Deep");
-                initialAutoPose = traj.getInitialPose(false).get();
-                yield drivetrain.followTrajectory(traj)
-                      .andThen(turret.aim());
-            }
-            default -> null;
-        };
+    private void setInitialAutoPose(Pose2d pose) {
+        initialAutoPose = pose;
     }
 
-    private Autos lastSelectedAuto;
-    private DriverStation.Alliance lastAlliance;
-
-    public Command getAutonomousCommand() {
-        Autos selected = autoChooser.get();
-        DriverStation.Alliance alliance = null;
-        if (Util.getAlliance().isPresent()) {
-            alliance = Util.getAlliance().get();
-        }
-        if (alliance != null && selected != null && (!alliance.equals(lastAlliance) || selected != lastSelectedAuto)) {
-            updateAuto(selected);
-            lastSelectedAuto = selected;
-            lastAlliance = alliance;
-        }
-        if (Robot.isSimulation() && initialAutoPose != null) {
-            drivetrain.resetPose(initialAutoPose);
-        }
-        return autonCommand;
+    private Command shootPreload() {
+        Command aimThenShootWhenReady = Commands.parallel(
+            Commands.parallel(turret.aim(), shooter.aim(), hood.aim()), // aim everything
+            Commands.sequence(
+                Commands.waitUntil(readyToShoot),
+                Commands.parallel(spinner.feed(), feeder.feed()))); // shoot
+        Command stopAimingAndShooting = Commands.parallel(
+            turret.off(),
+            shooter.off(),
+            hood.off(),
+            spinner.off(),
+            feeder.reverseAndOff()
+        );
+        return Commands.sequence(
+            aimThenShootWhenReady.withTimeout(15),
+            stopAimingAndShooting,
+            this.extender.armOut()
+        );
     }
+    
+    private AutoRoutine onePassBumpDepot() {
+        Command aimThenShootWhenReady = Commands.parallel(
+            Commands.parallel(turret.aim(), shooter.aim(), hood.aim()), // aim everything
+            Commands.sequence(
+                Commands.waitUntil(readyToShoot),
+                Commands.parallel(spinner.feed(), feeder.feed()))); // shoot
+        Command stopAimingAndShooting = Commands.parallel(
+            turret.off(),
+            shooter.off(),
+            hood.off(),
+            spinner.off(),
+            feeder.reverseAndOff()
+        );
+        
+        AutoRoutine routine = autoFactory.newRoutine("testRoutine");
+        
+        AutoTrajectory traj = routine.trajectory("Bump_Depot_Deep");
+        routine.active().onTrue(
+            Commands.sequence(
+                extender.armOut(),
+                roller.intake(),
+                traj.cmd()
+            )
+        );
 
-    private Trajectory<SwerveSample> loadTrajectory(String pathName) {
-        Optional<Trajectory<SwerveSample>> traj = Choreo.<SwerveSample>loadTrajectory(pathName);
-        // This should never be reached until the alliance is present
-        DriverStation.Alliance alliance = Util.getAlliance().get();
-        if (traj.isPresent()){
-            return alliance.equals(Alliance.Blue) ? traj.get() : traj.get().flipped();
-        }
-        return null;
+        traj.done().onTrue(
+            Commands.sequence(
+                aimThenShootWhenReady.withTimeout(5),
+                stopAimingAndShooting
+        ));
+
+        traj.getInitialPose().ifPresent(this::setInitialAutoPose);
+
+        return routine;
     }
 }
